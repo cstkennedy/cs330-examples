86b155ae130349543ba7a4861183a49997518333
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2017-02-20 12:00:47 -0500

    Added Example 5 to CPP Shapes (read function)

---
+++ Review-06-CPP-Shapes/Example-5/source/ShapeCollection.cpp
@@ -0,0 +1,80 @@
+#include <utility>
+#include <algorithm>
+#include <cassert>
+#include "ShapeCollection.h"
+
+/**
+ * 
+ */
+ShapeCollection::ShapeCollection()
+    :shapes() // Is this necessary?
+{
+}
+
+/**
+ * 
+ */
+ShapeCollection::ShapeCollection(int n)
+{       
+    shapes.reserve(n);
+}
+
+/**
+ * 
+ */
+ShapeCollection::ShapeCollection(const ShapeCollection& src)
+{
+    for (const Shape* s: src.shapes) {
+        shapes.push_back(s->clone());
+    }
+}
+
+/**
+ * 
+ */
+ShapeCollection::~ShapeCollection()
+{
+    for (Shape* s : this->shapes) {
+        delete s;
+    }
+}
+
+/**
+ *
+ */
+bool ShapeCollection::addShape(Shape* toAdd)
+{
+    assert(toAdd != nullptr);
+
+    shapes.push_back(toAdd);
+    return true;
+}
+
+/**
+ * 
+ */
+ShapeCollection& ShapeCollection::operator=(ShapeCollection rhs)
+{    
+    std::swap(*this, rhs);
+
+    return *this;
+}
+
+/**
+ * 
+ */
+void ShapeCollection::display(std::ostream& outs) const
+{
+    for (const Shape* s : this->shapes) {
+        assert(s != nullptr);
+        outs << *s << "\n";
+    }
+}
+
+
+void swap(ShapeCollection& lhs, ShapeCollection& rhs)
+{
+    using std::swap;
+
+    swap(lhs.shapes, rhs.shapes);
+}
---
+++ Review-06-CPP-Shapes/Example-5/source/runShapes.cpp
@@ -0,0 +1,105 @@
+// Thomas Kennedy
+// Review Example: Inheritance and the Factory Model
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#include "utilities.h"
+#include "shapeFactory.h"
+#include "ShapeCollection.h"
+
+/** @file */
+
+using namespace std;
+using namespace utilities;
+
+const string PROGRAM_HEADING[] = {
+    "Objects & Inheritance: 2-D Shapes",
+    "Thomas J. Kennedy"
+}; ///< Program Title 
+
+const int HEADING_LINES = 2; ///< Number of lines in Program Heading
+
+typedef RightTriangle       RhtTri; ///< Convenient shorthand for RightTriangle
+typedef EquilateralTriangle EqlTri; ///< Convenient shorthand for EquilateralTriangle
+
+/**
+ * Read shapes from an input stream
+ * and construct a `ShapeCollection` object
+ */
+ShapeCollection readShapes(std::istream& ins);
+
+
+/**
+ * This program accepts command line
+ * arguments
+ */
+int main(int argc, char** argv)
+{
+    if (argc < 2) {
+        cout << "No input file provided." << "\n";
+        cout << "Usage: " << argv[0] << " input_file" << "\n";
+        return 1;
+    }
+
+    ifstream shapesFile(argv[1]);
+
+    if (!shapesFile) {
+        cout << "Error: " << argv[1] << "could not be opened" << "\n";
+        return 2;
+    }
+
+    // Set formatting
+    cout.precision(4);
+    cout.setf(ios::fixed);
+
+    printProjectHeading(cout, PROGRAM_HEADING, HEADING_LINES);
+
+    // Examine the ShapeFactory
+    printHeading(cout, "Available Shapes", 38, '~');
+
+    // List the available shapes
+    ShapeFactory::listKnown(cout);
+    printHorizontalLine(cout, '-', 38);
+    cout << right << setw(2) 
+         << ShapeFactory::numberKnown() 
+         << " shapes available." 
+         << "\n";    
+
+    cout << "\n";
+
+    // Create 5 "Random" Shapes
+    ShapeCollection shapes = readShapes(shapesFile);
+
+    // Demonstrate assertions (review)
+    //shapes.addShape(ShapeFactory::createShape("1337 Haxor"));
+
+    // Print all the shapes
+    printHeading(cout, "Display All Shapes", 38, '~');
+    cout << shapes;
+
+    return 0;
+}
+
+/**
+ *
+ */
+ShapeCollection readShapes(std::istream& ins)
+{
+    ShapeCollection collection;
+    Shape*          s;
+
+    ins >> ws;
+
+    while(ins >> s) {
+        if (s != nullptr) {
+            collection.addShape(s);
+        }
+        ins >> ws;
+    }
+
+    cout << collection << "\n";
+
+    return collection;
+}
