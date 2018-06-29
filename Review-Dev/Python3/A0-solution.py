bf2eb6929aa9a502fd918031618c838c2b286b1b
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2018-06-28 21:59:21 -0400

    PEP8 style cleanup

--- Review-Dev/Python3/A0-solution.py
+++ Review-Dev/Python3/A0-solution.py
@@ -1,7 +1,7 @@
-#! /usr/bin/env python3     
+#! /usr/bin/env python3
 
-# Programmer : Thomas Kennedy      
-# Date       : 5 March 2014   
+# Programmer : Thomas Kennedy
+# Date       : 5 March 2014
 # Description: Lab Assignment 0 Solution
 
 import os
@@ -9,7 +9,8 @@ import sys
 import datetime
 
 # Global Variables
-W_WIDTH = 80; # Width of the terminal window
+W_WIDTH = 80  # Width of the terminal window
+
 
 def printHeading(title, width=W_WIDTH, outs=sys.stdout):
     """
@@ -26,67 +27,68 @@ def getUsername(first, last):
     Get the student's username from his or her first and last name
     """
 
-    return (first[0] + (last[:7] if len(last) > 7 else last )).lower()
+    return (first[0] + (last[:7] if len(last) > 7 else last)).lower()
 
 
 def main():
-    user_response = "N";     # Stores the student's response
+    user_response = "N"     # Stores the student's response
 
     # Print a fancy heading
     printHeading("CS150: Spring 2014 - Lab Assignment 1")
 
     # Get the student's first and last name
-    while user_response.upper() != "Y":    
+    while user_response.upper() != "Y":
         # Reset user response for each round.
-        user_response = "Q"    
+        user_response = "Q"
 
-        #Read the user's first and last name
+        # Read the user's first and last name
         first_name = input("Enter your first name: ").split()[0].title()
         last_name  = input("Enter your last name : ").split()[0].title()
 
         # Validate the student's name was entered correctly
-        while user_response.upper() not in ["Y","N"]:        
-            user_response = input("Confirm: Is your name {} {}? (Y/N): ".format( first_name, last_name ) )        
+        while user_response.upper() not in ["Y", "N"]:
+            user_response = input("Confirm: Is your name {} {}? (Y/N): ".format(first_name, last_name))
 
-        # If the student's name is correct, stop the loop, otherwise print(a message and go back to the top.
-        if user_response == "N":        
+        # If the student's name is correct, stop the loop,
+        # otherwise print(a message and go back to the top.
+        if user_response == "N":
             print("Input Error: Please try again.")
 
     # Print a line to the screen
     print("*" * W_WIDTH)
 
     # Get the student's birth year.
-    birth_year = int( input("Enter the year you were born: ") )
+    birth_year = int(input("Enter the year you were born: "))
 
     # Print a line to the screen
     print("*" * W_WIDTH)
 
     # Calculate and save the username
-    username = getUsername(first_name, last_name);
+    username = getUsername(first_name, last_name)
 
     # Calculate and save the age
-    age = int( datetime.date.today().strftime("%Y") ) -  birth_year
+    age = int(datetime.date.today().strftime("%Y")) -  birth_year
 
-    print("Your CS username is most likely {}".format( username ))
-    print(("You are probably turning {} this year" + ("... that's unusual." if age < 16 or age > 50 else ".")).format( age ))
+    print("Your CS username is most likely {}".format(username))
+    print(("You are probably turning {} this year" + ("... that's unusual." if age < 16 or age > 50 else ".")).format(age))
 
     # File Output
-    filename = username + "-lab-assignment-0.txt";
-    print("Writing output to {}.\nYou can find it in the same folder as the PY.".format( filename ))
+    filename = username + "-lab-assignment-0.txt"
+    print("Writing output to {}.\nYou can find it in the same folder as the PY.".format(filename))
 
-    #open the output file
+    # Open the output file
     try:
-        out_file = open(filename, "w");
+        out_file = open(filename, "w")
     except:
         print("\nError: Could not open file.\n")
         sys.exit(1)
-    
-    printHeading(first_name + "'s Lab Assignment 1", W_WIDTH, out_file);
 
-    print("{:<20}: {:<20}".format( "First Name",     first_name ), file=out_file)
-    print("{:<20}: {:<20}".format( "Last Name",      last_name ), file=out_file)
-    print("{:<20}: {:<20}".format( "Today's Date",   datetime.date.today().strftime("%d %A %Y") ), file=out_file)
-    print("{:<20}: {:<20}".format( "Estimated Age:", age ), file=out_file)
+    printHeading(first_name + "'s Lab Assignment 1", W_WIDTH, out_file)
+
+    print("{:<20}: {:<20}".format("First Name", first_name), file=out_file)
+    print("{:<20}: {:<20}".format("Last Name", last_name), file=out_file)
+    print("{:<20}: {:<20}".format("Today's Date", datetime.date.today().strftime("%d %A %Y")), file=out_file)
+    print("{:<20}: {:<20}".format("Estimated Age:", age), file=out_file)
 
     print("*" * W_WIDTH, file=out_file)
 
@@ -94,4 +96,4 @@ def main():
 
 
 if __name__ == "__main__":
-     main()
+    main()
--- Review-Dev/Python3/A1-solution.py
+++ Review-Dev/Python3/A1-solution.py
@@ -1,8 +1,10 @@
-#! /usr/bin/env python                          
+#! /usr/bin/env python
+
 
 import os
 import sys
 import random
+
 
 #
 # Print a stylized heading
@@ -12,44 +14,45 @@ def printHeading(title):
     print(title.center(40))
     print("=" * 40)
 
-#Main Function
-def main(): 
-    printHeading( "CS 150 Lab Spring 2014" )   
 
-    #Read num_2 and num_2
-    num_1 = int( input( "Enter an Integer: " ) )
-    num_2 = int( input( "Enter an Integer: " ) )
-    
-    #Perform Calcualations
-    i_addition       = num_1 + num_2;
-    i_subtraction    = num_1 - num_2;
-    i_multiplication = num_1 * num_2;
-    i_division       = num_1 / num_2;
-    i_modulus        = num_1 % num_2; # % is the remainder from integer division
+# Main Function
+def main():
+    printHeading("CS 150 Lab Spring 2014")
 
-    printHeading( "Integer Arithmetic Results")
-    print("{:<18}: {:>20}".format("Addition",       i_addition))
-    print("{:<18}: {:>20}".format("Subtraction",    i_subtraction))
+    # Read num_2 and num_2
+    num_1 = int(input("Enter an Integer: "))
+    num_2 = int(input("Enter an Integer: "))
+
+    # Perform Calcualations
+    i_addition       = num_1 + num_2
+    i_subtraction    = num_1 - num_2
+    i_multiplication = num_1 * num_2
+    i_division       = num_1 / num_2
+    i_modulus        = num_1 % num_2  # % is the remainder from int division
+
+    printHeading("Integer Arithmetic Results")
+    print("{:<18}: {:>20}".format("Addition", i_addition))
+    print("{:<18}: {:>20}".format("Subtraction", i_subtraction))
     print("{:<18}: {:>20}".format("Multiplication", i_multiplication))
-    print("{:<18}: {:>20}".format("Division",       i_division))
-    print("{:<18}: {:>20}".format("Modulus",        i_modulus))
+    print("{:<18}: {:>20}".format("Division", i_division))
+    print("{:<18}: {:>20}".format("Modulus", i_modulus))
 
-    #print a blank line
+    # Print a blank line
     print("")
 
-    num_3 = float(input( "Enter a Real Number: " ))
-    num_4 = float(input( "Enter a Real Number: " ))
-    
-    d_addition       = num_3 + num_4;
-    d_subtraction    = num_3 - num_4;
-    d_multiplication = num_3 * num_4;
-    d_division       = num_3 / num_4;
+    num_3 = float(input("Enter a Real Number: "))
+    num_4 = float(input("Enter a Real Number: "))
 
-    printHeading( "Real Arithmetic Results")
-    print("{:<18}: {:>20.2f}".format("Addition",       d_addition))
-    print("{:<18}: {:>20.2f}".format("Subtraction",    d_subtraction))
+    d_addition       = num_3 + num_4
+    d_subtraction    = num_3 - num_4
+    d_multiplication = num_3 * num_4
+    d_division       = num_3 / num_4
+
+    printHeading("Real Arithmetic Results")
+    print("{:<18}: {:>20.2f}".format("Addition", d_addition))
+    print("{:<18}: {:>20.2f}".format("Subtraction", d_subtraction))
     print("{:<18}: {:>20.2f}".format("Multiplication", d_multiplication))
-    print("{:<18}: {:>20.2f}".format("Division",       d_division))
+    print("{:<18}: {:>20.2f}".format("Division", d_division))
 
     print("")
     temp_f = float(input("Enter a Temperature (F): "))
@@ -57,10 +60,10 @@ def main():
     temp_c = (5.0/9) * (temp_f - 32)
     temp_k = temp_c + 273.15
 
-    printHeading( "Temperature Results")
-    print("{:<18}: {:>20.2f}".format("Farenheit",  temp_f))
-    print("{:<18}: {:>20.2f}".format("Celcius",    temp_c)) 
-    print("{:<18}: {:>20.2f}".format("Kelvin",  temp_k))
+    printHeading("Temperature Results")
+    print("{:<18}: {:>20.2f}".format("Farenheit", temp_f))
+    print("{:<18}: {:>20.2f}".format("Celcius", temp_c))
+    print("{:<18}: {:>20.2f}".format("Kelvin", temp_k))
 
 if __name__ == "__main__":
-     main()
+    main()
