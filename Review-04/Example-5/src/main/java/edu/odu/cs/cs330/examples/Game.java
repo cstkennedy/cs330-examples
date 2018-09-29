7916774b7aa80a950ecdc6e8f835bfd41fc761bd
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2018-09-29 10:21:30 -0400

    Add Example 5 with reasonable Board Unit Test

---
+++ Review-04/Example-5/src/main/java/edu/odu/cs/cs330/examples/Game.java
@@ -0,0 +1,169 @@
+package edu.odu.cs.cs330.examples;
+
+import java.io.IOException;
+
+/**
+ * Orchestrates a single match of Tic-Tac-Toe.
+ */
+public class Game
+{
+    private Board board;
+    private Referee ref; // The referee for this game
+
+    private Player player1;
+    private Player player2;
+
+    private Player winner;
+
+    private int numMovesMade;
+
+    // Disable Game Default Constructor
+    private Game()
+    {
+
+    }
+
+    /**
+     * Construct a Game setting player 1 and player 2.
+     */
+    public Game(Player p1, Player p2)
+    {
+        player1 = p1;
+        player2 = p2;
+
+        board = new Board();
+        ref = new Referee(this.board);
+
+        player1.setSymbol('X');
+        player2.setSymbol('O');
+    }
+
+    /**
+     * Play one round of Tic-Tac-Toe.
+     *
+     * @return true if the game ended during the round
+     *
+     * @throws IOException if there is an error reading the selected move
+     */
+    public boolean playRound()
+        throws IOException
+    {
+        // The game ended already - assert could be used
+        if (board.isFull()) {
+            return true;
+        }
+
+        int winnerId = 0;
+
+        System.out.println(board);
+        roundTurn(player1);
+
+        // The game is over
+        if (board.isFull()) {
+            System.out.println(board);
+            return true;
+        }
+
+        winnerId = ref.checkForWin();
+
+        if (winnerId == 1) {
+            winner = player1;
+            return true;
+        }
+
+        System.out.println(board);
+        roundTurn(player2);
+
+        // Final board
+        System.out.println(board);
+
+        winnerId = ref.checkForWin();
+
+        if (winnerId == 2) {
+            winner = player2;
+            return true;
+        }
+
+        return false;
+    }
+
+    public Player getPlayer1()
+    {
+        return player1;
+    }
+
+    public Player getPlayer2()
+    {
+        return player2;
+    }
+
+    public Player getWinner()
+    {
+        return winner;
+    }
+
+    public Player getLoser()
+    {
+        // Stalemate
+        if (endedWithStalemate()) {
+            //return Player::referenceCylon;
+            return null;
+        }
+
+        // There was a win, figure out who lost
+        if (winner.equals(player1)) {
+            return player2;
+        }
+
+        return player1;
+    }
+
+    public boolean endedWithWin()
+    {
+        return (winner != null);
+    }
+
+    public boolean endedWithStalemate()
+    {
+        return (board.isFull()) && (winner == null);
+    }
+
+    public boolean isOver()
+    {
+        return (endedWithWin() || endedWithStalemate());
+    }
+
+    public boolean isNotOver()
+    {
+        return !isOver();
+    }
+
+    public Board getBoard()
+    {
+        return board;
+    }
+
+    /**
+     * Get a player move, and update the board.
+     */
+    private boolean roundTurn(Player player)
+        throws IOException
+    {
+        int   move;
+        char  sym;
+
+        move = player.nextMove();
+        sym  = player.getSymbol();
+
+        //while (board.getCell(move) != 'X' && board.getCell(move) != 'O') {
+        while (!ref.selectedCellIsEmpty(move)) {
+            move = player.nextMove();
+            sym  = player.getSymbol();
+        }
+        board.setCell(move, sym);
+
+        // @todo add move validation
+        return true;
+    }
+
+};
---
+++ Review-04/Example-5/src/main/java/edu/odu/cs/cs330/examples/Player.java
@@ -0,0 +1,160 @@
+package edu.odu.cs.cs330.examples;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * This is more a Player interface than a Player class.
+ * <p>
+ * However, such distinctions and discussions belong in
+ * the OOP and Inheritance Modules
+ */
+class Player {
+    /**
+     * Message used to prompt a human player for a move
+     */
+    private static final String PROMPT_MSG = "Enter your desired move (1-9): ";
+
+    private String name;
+    private char   symbol;
+
+    /**
+     * A Player that serves as a sentinal value or placeholder
+     */
+    public static final Player REFERENCE_CYLON = new Player();
+
+    /**
+     * Checks whether a player is a placeholder or
+     * an actual player.
+     *
+     * @param possibleCylon the player whose humanity is in question
+     *
+     * @return true if the player is a Cylon
+     */
+    public static boolean isGeneric(final Player possibleCylon)
+    {
+        return possibleCylon.equals(Player.REFERENCE_CYLON);
+    }
+
+    /**
+     * Create a Player with a Generic name
+     */
+    public Player()
+    {
+        name = "I. C. Generic";
+    }
+
+    /**
+     * Create a Player with a selected name
+     *
+     * @param n desired name
+     */
+    public Player(String n)
+    {
+        name = n;
+    }
+
+    /**
+     * Retrieve name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Set player name.
+     *
+     * @param n new name
+     *
+     * @pre (n.size() > 0)
+     */
+    public void setName(String n)
+    {
+        name  = n;
+    }
+
+    /**
+     * Retrieve the next move.
+     */
+    public int nextMove()
+        throws IOException
+    {
+        Scanner scnr = new Scanner(System.in);
+
+        int choice;
+
+        System.out.print(name + ", " + Player.PROMPT_MSG);
+        choice = scnr.nextInt();
+
+        return choice;
+    }
+
+    /**
+     * Is this a Human Player?
+     * <p>
+     * In this discussion, always yes :(
+     */
+    public boolean isHuman()
+    {
+        return true;
+    }
+
+    /**
+     * Is this a Computer Player?
+     * <p>
+     * In this discussion, always no :(
+     */
+    public boolean isComputer()
+    {
+        return false;
+    }
+
+    /**
+     * Retrieve player symbol to be used
+     * for marking moves.
+     */
+    public char getSymbol()
+    {
+        return symbol;
+    }
+
+    /**
+     * Change the player symbol.
+     *
+     * @param newSymbol new character to be used by the player
+     */
+    public void setSymbol(char newSymbol)
+    {
+        symbol = newSymbol;
+    }
+
+    @Override
+    public boolean equals(Object rhs)
+    {
+        if (!(rhs instanceof Player)) {
+            return false;
+        }
+
+        Player rhsP = (Player) rhs;
+
+        return this.name.equals(rhsP.name);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return this.name.hashCode();
+    }
+
+    /**
+     * Generate a player string, but only the name.
+     */
+    @Override
+    public String toString()
+    {
+        return this.getName();
+    }
+
+}
+
