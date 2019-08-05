9a6ed927374f31248bc317da0097d5cf52e5afc4
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2019-08-05 18:17:29 -0400

    Add tox to Python examples with unit tests

---
+++ Review-04/Example-6/src/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='CS 330 Tic-Tac-Toe',
+      version='1.0',
+      description='Python Tic-Tac-Toe Example',
+      author='Thomas J. Kennedy',
+      author_email='tkennedy@cs.odu.edu')
