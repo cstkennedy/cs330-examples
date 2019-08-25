152d0e6d97befe69aaa348104a672c28e735bb23
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2019-08-25 15:41:36 -0400

    Draft first iteration of python deploy replacement script

---
+++ deploy-all.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import zipfile
+import glob
+import subprocess
+
+from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor
+
+
+def prepare_example_set(example_set: str):
+    """
+    Prepare an Example set by running cleanup operations, then documentation
+    generation.
+
+    # Args
+        example_set: directory containing one or more examples
+    """
+
+    for example_dir in glob.iglob(f"{example_set}/Example*"):
+
+        if os.path.isfile(f"{example_dir}/makefile"):
+            print(f"{example_dir} -> clean")
+            try:
+                subprocess.run(f"make -C {example_dir} clean".split(),
+                               stdout=subprocess.DEVNULL,
+                               stderr=subprocess.DEVNULL)
+
+            except subprocess.CalledProcessError as err:
+                print(f"{example_dir} -> Already clean")
+
+            print(f"{example_dir} -> docs")
+            subprocess.run(f"make docs".split(),
+                           stdout=subprocess.DEVNULL,
+                           stderr=subprocess.DEVNULL,
+                           cwd=example_dir)
+
+        else:
+            print(f"{example_dir} -> No Makefile found.")
+
+
+def main():
+    try:
+        base_review_dir = sys.argv[1]
+
+    except IndexError as err:
+        base_review_dir = "."
+
+    build_dir = f"{base_review_dir}/build"
+
+    review_dirs = glob.glob(f"{base_review_dir}/Review-*")
+
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        for review_dir in review_dirs:
+            executor.submit(prepare_example_set, review_dir)
+
+    for review_dir in review_dirs:
+        zip_name = build_dir + "/" + review_dir.split("/")[-1] + ".zip"
+
+        print(zip_name, "->", review_dir)
+
+        with zipfile.ZipFile(zip_name, "w") as review_zip:
+            for root_dir, _, files in os.walk(review_dir):
+                for a_file in files:
+                    review_zip.write(os.path.join(root_dir, a_file))
+
+
+if __name__ == "__main__":
+    main()
+
+
