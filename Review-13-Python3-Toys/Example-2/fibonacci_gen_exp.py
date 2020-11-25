3178cb248277b0c05f76f0df358a84bcdbb0fe13
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2020-11-25 16:52:58 -0500

    Update Python toy examples (e.g., f-strings)

--- Review-13-Python3-Toys/Example-2/fibonacci_gen_exp.py
+++ Review-13-Python3-Toys/Example-2/fibonacci_gen_exp.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import sys
 
 def fibonacci_sequence(sequence_length):
     """
@@ -10,9 +11,15 @@ def fibonacci_sequence(sequence_length):
     no greater than 20
     """
 
+    # De-Morgan's Law
+    # !(index >= 3 && index <= 200)
+    # !(index >= 3) || !(index <= 200)
+    # (index < 3 || index > 200)
+    if sequence_length < 3 or sequence_length > 200:
+        raise ValueError(f"{sequence_length:3d} is not between 3 and 200\n")
+
     fm2 = 1  # n-2 (previous previous) fibonacci number
     fm1 = 1  # n-1 (previous) fibonacci number
-    f = 0  # current fibonacci number
 
     yield fm2
     yield fm1
@@ -25,22 +32,30 @@ def fibonacci_sequence(sequence_length):
         yield f
 
 
+def get_fibonacci_index():
+    """
+    Check the command line arguments (i.e., sys.argv) for an index. If no index
+    was provided prompt the user.
+
+    Returns:
+        Fibonacci index (i.e., how many numbers to generate).
+    """
+
+    if len(sys.argv) < 2:
+        index = input("Generate how many numbers? ")
+        index = int(index)  # loosely typed
+        print()
+
+    else:
+        index = int(sys.argv[1])
+
+    return index
+
+
 def main():
-    index = input("Generate how many numbers? ")
-    index = int(index)  # loosely typed
-
-    print()
-
-    # De-Morgan's Law
-    # !(index >= 3 && index <= 200)
-    # !(index >= 3) || !(index <= 200)
-    # (index < 3 || index > 200)
-    if index < 3 or index > 200:
-        print(f"{index:3d} is not between 3 and 200\n")
-        exit(1)
+    index = get_fibonacci_index()
 
     # Think of the tuple (idx, f) as std::pair<int, long>
-
     for idx, f in enumerate(fibonacci_sequence(index), start=1):
         print(f"{idx:>3d}: {f:10d}")
 
--- Review-13-Python3-Toys/Example-3/coin_flip.py
+++ Review-13-Python3-Toys/Example-3/coin_flip.py
@@ -6,16 +6,18 @@ import random
 from datetime import datetime
 from concurrent.futures import ProcessPoolExecutor
 
+from typing import Dict
+
 DEFAULT_NUM_TRIALS = 10000
 SUMMARY_FMT_STR = "# Heads: {:>6d} ({:>6.4f}) / # Tails {:>6d} ({:>6.4f})"
 
 
-def simulate_flips(num_trials):
+def simulate_flips(num_trials: int) -> Dict[str, int]:
     """
     Simulate a specified number of coin flips
 
-    Keyword arguments:
-        num_trials -- number of trials to attempt
+    Args:
+        num_trials: number of trials to attempt
     """
 
     counts = {"Heads": 0, "Tails": 0}
@@ -29,13 +31,13 @@ def simulate_flips(num_trials):
     return counts
 
 
-def print_summary(counts):
+def print_summary(counts: Dict[str, int]):
     """
     Print coin flip statistics (including probability distribution)
 
-    Keyword arguments:
-        counts -- heads and tails counts in the form
-                  {"Heads": int, "Tails": int}
+    Args:
+        counts: heads and tails counts in the form
+                {"Heads": int, "Tails": int}
     """
 
     total_trials = sum(counts.values())
@@ -46,26 +48,26 @@ def print_summary(counts):
                                  float(counts["Tails"]) / total_trials))
 
 
-def run_parallel(num_workers, num_trials):
+def run_parallel(num_workers: int, num_trials: int):
     """
     Run coin flip simulations in parallel using Python
     Processes
     """
 
-    jobs_per_thread = num_trials // num_workers
-    jobs_for_last = jobs_per_thread + (num_trials % num_workers)
+    flips_per_thread = num_trials // num_workers
+    flips_for_last = flips_per_thread + (num_trials % num_workers)
 
-    futures = list()
+    futures = []
 
     with ProcessPoolExecutor(max_workers=num_workers) as executor:
-        for _ in [jobs_per_thread] * (num_workers - 1) + [jobs_for_last]:
-            futures.append(executor.submit(simulate_flips, jobs_per_thread))
+        for worker_flips in [flips_per_thread] * (num_workers - 1) + [flips_for_last]:
+            futures.append(executor.submit(simulate_flips, worker_flips))
 
     total_heads = 0
     total_tails = 0
     for idx, future in enumerate(futures):
         result = future.result()
-        print("Worker {:>2d} -> ".format(idx), end="")
+        print(f"Worker {idx:>2d} -> ", end="")
         print_summary(result)
 
         total_heads += result["Heads"]
@@ -75,14 +77,14 @@ def run_parallel(num_workers, num_trials):
 
     print("Overall   -> ", end="")
     print_summary({"Heads": total_heads, "Tails": total_tails})
-    print("Total Trials -> %d" % (total_heads + total_tails))
+    print(f"Total Trials -> {(total_heads + total_tails)}")
 
 
 def main():
     try:
         num_procs = int(sys.argv[1])
 
-    except (IndexError, ValueError) as err:
+    except (IndexError, ValueError) as _err:
         num_procs = 1
 
     if num_procs > 32:
@@ -91,7 +93,7 @@ def main():
     try:
         num_trials = int(sys.argv[2])
 
-    except (IndexError, ValueError) as err:
+    except (IndexError, ValueError) as _err:
         num_trials = DEFAULT_NUM_TRIALS
 
     if num_procs == 1:
