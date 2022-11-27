7aa61435f56172a1c52f2c0348637cd34be28552
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2022-11-27 10:07:38 -0500

    Add Python Generator Discussion

---
+++ Review-13-Python-Design-Discussion/Generators-1/process_words.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+
+from __future__ import annotations
+
+import sys
+
+
+# 9.2 ms …  13.4 ms
+# 9.1 ms …  13.2 ms
+# 9.2 ms …  12.7 ms
+# 9.1 ms …  13.6 ms
+# 9.6 ms …  15.9 ms
+
+def word_stream(word_fname: str):
+    """
+    T.B.W.
+    """
+    with open(word_fname, "r") as word_file:
+        for line in word_file:
+            word = line.strip()
+            if word:
+                yield word
+
+def contains_chars(word: str, banned: list[str] = None) -> bool:
+    if not banned:
+        banned = [".", "+"]
+
+    for char in banned:
+        if char in word:
+            return True
+
+    return False
+
+
+def main():
+
+    word_fname = sys.argv[1]
+
+    words = word_stream(word_fname)
+    #  words_lt_5 = (word for word in words if len(word) < 5)
+    words = filter(lambda word: len(word) < 5, words)
+    words = filter(lambda word: not contains_chars(word), words)
+
+
+    for word in words:
+        print(f"|{word}|")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Review-13-Python-Design-Discussion/README.md
@@ -0,0 +1,13 @@
+This is a discussion of selected problems from previous reviews revisited
+in Python 3. We will briefly discuss:
+
+  - Generator Expressions
+  - Dictionary Comprehensions
+  - Modules
+  - `enumerate`
+  - `pydoc`
+
+
+The recorded discussions are available at:
+
+  - Generators-1: <https://youtu.be/xQsvClZaG80>
