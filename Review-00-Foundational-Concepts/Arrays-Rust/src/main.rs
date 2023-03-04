d48ce566621de9dc27ca2cf1c90e735d45b4c099
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2023-03-03 21:20:46 -0500

    Tweak initial Rust Array Example

--- Review-00-Foundational-Concepts/Arrays-Rust/src/main.rs
+++ Review-00-Foundational-Concepts/Arrays-Rust/src/main.rs
@@ -1,23 +1,18 @@
-/**
- * Maximum allowed array size
- */
+/// Maximum allowed array size
 const MAX_SIZE: usize = 10;
 
-/**
- * 20 Dash Divider
- */
+/// 20 Dash Divider
 const DIVIDER: &'static str = "--------------------";
 
 fn main() {
     static_array_demo();
 }
 
-/**
- * Statically allocated array demo - keep track of what is actually used
- */
-fn static_array_demo()
-{
-    let mut names: [&str; MAX_SIZE] = ["" ;MAX_SIZE];
+///
+/// Statically allocated array demo - keep track of what is actually used
+///
+fn static_array_demo() {
+    let mut names: [&str; MAX_SIZE] = [""; MAX_SIZE];
     let mut used: usize = 0;
 
     names[used] = "Thomas";
@@ -34,14 +29,12 @@ fn static_array_demo()
     print_array(&names);
 }
 
-/**
- * Print an array using the usual index based for loop.
- *
- * @param toPrint the array to print
- * @param length size of the array (number of elements)
- */
-fn print_array(to_print: &[&str])
-{
+///Print an array using the usual index based for loop.
+///
+/// @param toPrint the array to print
+/// @param length size of the array (number of elements)
+///
+fn print_array(to_print: &[&str]) {
     for value in to_print.iter() {
         println!("{}", value);
     }
