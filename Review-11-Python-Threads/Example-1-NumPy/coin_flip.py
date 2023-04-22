17192182476b326da2c613e94630130d84ba88ae
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2023-04-22 11:42:20 -0400

    Add NumPy Examples

---
+++ Review-11-Python-Threads/Example-1-NumPy/coin_flip.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python3
+
+from __future__ import annotations
+
+import sys
+import random
+import numpy as np
+
+from datetime import datetime
+from concurrent.futures import ProcessPoolExecutor
+
+DEFAULT_NUM_TRIALS = 10000
+SUMMARY_FMT_STR = "# Heads: {:>6d} ({:>6.4f}) / # Tails {:>6d} ({:>6.4f})"
+
+
+def simulate_flips(num_trials: int) -> dict[str, int]:
+    """
+    Simulate a specified number of coin flips
+
+    Args:
+        num_trials: number of trials to attempt
+    """
+
+    flips = np.random.randint(2, size=num_trials)
+
+    num_heads = np.count_nonzero(flips)
+    counts = {"Heads": num_heads, "Tails": num_trials - num_heads}
+
+    return counts
+
+
+def print_summary(counts: dict[str, int]):
+    """
+    Print coin flip statistics (including probability distribution)
+
+    Args:
+        counts: heads and tails counts in the form
+                {"Heads": int, "Tails": int}
+    """
+
+    total_trials = sum(counts.values())
+
+    print(SUMMARY_FMT_STR.format(counts["Heads"],
+                                 float(counts["Heads"]) / total_trials,
+                                 counts["Tails"],
+                                 float(counts["Tails"]) / total_trials))
+
+
+def run_parallel(num_workers: int, num_trials: int):
+    """
+    Run coin flip simulations in parallel using Python
+    Processes
+    """
+
+    flips_per_thread = num_trials // num_workers
+    flips_for_last = flips_per_thread + (num_trials % num_workers)
+
+    futures = []
+
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        for worker_flips in [flips_per_thread] * (num_workers - 1) + [flips_for_last]:
+            futures.append(executor.submit(simulate_flips, worker_flips))
+
+    total_heads = 0
+    total_tails = 0
+    for idx, future in enumerate(futures):
+        result = future.result()
+        print(f"Worker {idx:>2d} -> ", end="")
+        print_summary(result)
+
+        total_heads += result["Heads"]
+        total_tails += result["Tails"]
+
+    print("-" * 72)
+
+    print("Overall   -> ", end="")
+    print_summary({"Heads": total_heads, "Tails": total_tails})
+    print(f"Total Trials -> {(total_heads + total_tails)}")
+
+
+def main():
+    try:
+        num_procs = int(sys.argv[1])
+
+    except (IndexError, ValueError) as _err:
+        num_procs = 1
+
+    if num_procs > 32:
+        num_procs = 32
+
+    try:
+        num_trials = int(sys.argv[2])
+
+    except (IndexError, ValueError) as _err:
+        num_trials = DEFAULT_NUM_TRIALS
+
+    if num_procs == 1:
+        results = simulate_flips(num_trials)
+        print_summary(results)
+
+    else:
+        time_parallel = datetime.now()
+        run_parallel(num_procs, num_trials)
+        time_parallel = (datetime.now() - time_parallel).total_seconds()
+
+        print()
+        #  print("Parallel Time: %f" % time_parallel)
+        #  print("Parallel Time: {}".format(time_parallel))
+        print(f"Parallel Time: {time_parallel:}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Review-12-Python-Patterns/Example-1-NumPy-Better/prime_number.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+
+# Programmer : Thomas J. Kennedy
+
+import sys
+
+import prime.bruteforce as bruteforce
+
+
+PROGRAM_HEADING = (
+    "Prime Number Generation",
+    "Thomas J. Kennedy"
+)
+
+
+def __parse_args():
+    """
+    Parse command line arguments (num_primes). Default to 10.
+    """
+
+    try:
+        num_primes = int(sys.argv[1])
+
+    except (IndexError, ValueError) as _err:
+        num_primes = 10
+
+    return num_primes
+
+
+def main():
+    """
+    The main function. In practice I could name this
+    anything. The name main was selected purely
+    out of familiarity.
+
+    The "if __name__" line below determines what runs
+
+    """
+
+    # Print Program Heading
+    print("-" * 80)
+    for line in PROGRAM_HEADING:
+        print(f"{line:^80}")
+    print("-" * 80)
+
+    num_primes = __parse_args()
+
+
+    for prime_num in bruteforce.generate_primes(num_primes):
+        print(prime_num)
+
+
+if __name__ == "__main__":
+    main()
