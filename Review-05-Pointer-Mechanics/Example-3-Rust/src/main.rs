c226844c4a605b3b509c8000f67fce126cd81ba9
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2024-02-12 19:57:36 -0500

    Code cleanup

--- Review-05-Pointer-Mechanics/Example-3-Rust/src/main.rs
+++ Review-05-Pointer-Mechanics/Example-3-Rust/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let D_LINE = std::iter::repeat("-").take(48).collect::<String>();
+    let d_line = "-".repeat(48);
 
-    const num_powers: usize = 32;
+    const NUM_POWERS: usize = 32;
 
-    let mut powers_of_two = [0i32; num_powers];
+    let mut powers_of_two = [0i32; NUM_POWERS];
 
-    println!("{}", D_LINE);
+    println!("{}", d_line);
 
     for i in 0..powers_of_two.len() {
-        powers_of_two[i] = (1 << i);
+        powers_of_two[i] = 1 << i;
     }
 
     for val in powers_of_two.iter() {
@@ -16,30 +16,42 @@ fn main() {
     }
 
     println!();
-    println!("{}", D_LINE);
+    println!("{}", d_line);
     println!();
 
     for i in 0..powers_of_two.len() {
-        println!("{:>2}: {:>11} {:>32b}", i, powers_of_two[i], powers_of_two[i]);
+        println!(
+            "{:>2}: {:>11} {:>32b}",
+            i, powers_of_two[i], powers_of_two[i]
+        );
     }
 
     println!();
-    println!("{}", D_LINE);
+    println!("{}", d_line);
     println!();
 
     for i in 0..powers_of_two.len() {
-        println!("{:>2}: {:>11} {:>32p}", i, powers_of_two[i], &powers_of_two[i]);
+        println!(
+            "{:>2}: {:>11} {:>32p}",
+            i, powers_of_two[i], &powers_of_two[i]
+        );
     }
 
-    let size_in_bytes = std::mem::size_of::<u32>();
+    let size_in_bytes = std::mem::size_of::<i32>();
     let size_in_bits = size_in_bytes << 3;
 
     println!();
-    println!("size_of(u32): {} bytes / {} bits", size_in_bytes, size_in_bits);
+    println!(
+        "size_of(i32): {} bytes / {} bits",
+        size_in_bytes, size_in_bits
+    );
 
     let size_in_bytes = std::mem::size_of_val(&powers_of_two);
     let size_in_bits = size_in_bytes << 3;
 
     println!();
-    println!("size_of([u32; 32]): {} bytes / {} bits", size_in_bytes, size_in_bits);
+    println!(
+        "size_of([i32; 32]): {} bytes / {} bits",
+        size_in_bytes, size_in_bits
+    );
 }
