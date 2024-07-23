f7125f3fe97a8b93f9eced9c22f6ece2f18415f0
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2024-07-23 14:57:11 -0400

    Add first round of tests

--- Review-14-Python-Builder-Pattern/Example-2/tests/test_room_builder.py
+++ Review-14-Python-Builder-Pattern/Example-2/tests/test_room_builder.py
@@ -1,29 +1,75 @@
 import pytest
 from hamcrest import *
 
+from room import Room, RoomBuilder
+
 
 def test_name_not_set():
-    pytest.fail("Test not yet written")
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_dimensions(1, 1)
+            .with_flooring("Tile", 1.0)
+            .build()
+        )
+        # fmt: on
 
 
-def test_name_too_short():
-    pytest.fail("Test not yet written")
+@pytest.mark.parametrize("some_name", ["", "a", "ab"])
+def test_name_too_short(some_name):
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_name(some_name)
+            .with_dimensions(1, 1)
+            .with_flooring("Tile", 1.0)
+            .build()
+        )
+        # fmt: on
 
 
 def test_flooring_type_not_set():
-    pytest.fail("Test not yet written")
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_name("Generic Room")
+            .with_dimensions(1, 1)
+            .build()
+        )
+        # fmt: on
 
 
-def test_flooring_type_too_short():
-    pytest.fail("Test not yet written")
+@pytest.mark.parametrize("some_name", ["", "a", "ab"])
+def test_flooring_type_too_short(some_name):
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_name("Generic Room")
+            .with_dimensions(1, 1)
+            .with_flooring(some_name, 1.0)
+            .build()
+        )
+        # fmt: on
 
 
 def test_flooring_cost_not_set():
-    pytest.fail("Test not yet written")
+    pytest.skip("This should never happen if type is set")
 
 
 def test_length_not_set():
-    pytest.fail("Test not yet written")
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_name("Generic Name")
+            .with_flooring("Tile", 1.0)
+            .build()
+        )
+        # fmt: on
 
 
 def test_width_not_set():
@@ -31,28 +77,94 @@ def test_width_not_set():
 
 
 def test_flooring_cost_is_zero():
-    pytest.fail("Test not yet written")
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_name("Generic Name")
+            .with_dimensions(1, 1)
+            .with_flooring("Tile", 0)
+            .build()
+        )
+        # fmt: on
 
 
 def test_flooring_cost_is_negative():
-    pytest.fail("Test not yet written")
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_name("Generic Name")
+            .with_dimensions(1, 1)
+            .with_flooring("Tile", -1)
+            .build()
+        )
+        # fmt: on
 
 
 def test_length_is_zero():
-    pytest.fail("Test not yet written")
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_name("Generic Name")
+            .with_dimensions(0, 1)
+            .with_flooring("Tile", -1)
+            .build()
+        )
+        # fmt: on
 
 
 def test_length_is_negative():
-    pytest.fail("Test not yet written")
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_name("Generic Name")
+            .with_dimensions(-1, 1)
+            .with_flooring("Tile", -1)
+            .build()
+        )
+        # fmt: on
 
 
 def test_width_is_zero():
-    pytest.fail("Test not yet written")
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_name("Generic Name")
+            .with_dimensions(1, 0)
+            .with_flooring("Tile", -1)
+            .build()
+        )
+        # fmt: on
 
 
 def test_width_is_negative():
-    pytest.fail("Test not yet written")
+    with pytest.raises(ValueError):
+        # fmt: off
+        _ = (
+            RoomBuilder()
+            .with_name("Generic Name")
+            .with_dimensions(1, -1)
+            .with_flooring("Tile", -1)
+            .build()
+        )
+        # fmt: on
 
 
 def test_build_success():
-    pytest.fail("Test not yet written")
+    room = (
+        RoomBuilder()
+        .with_name("Generic Name")
+        .with_dimensions(1, 2)
+        .with_flooring("Tile", 1)
+        .build()
+    )
+
+    assert_that(room.name, is_(equal_to("Generic Name")))
+    assert_that(room.dimensions.length, is_(close_to(1, 1e-8)))
+    assert_that(room.dimensions.width, is_(close_to(2, 1e-8)))
+    assert_that(room.flooring.type_name, is_(equal_to("Tile")))
+    assert_that(room.flooring.unit_cost, is_(close_to(1, 1e-2)))
