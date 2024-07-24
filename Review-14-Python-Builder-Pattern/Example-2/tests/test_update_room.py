021dade4a41e264d2b1393b01ba4ab9e06f65b91
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2024-07-24 11:43:33 -0400

    Add third round of tests

--- Review-14-Python-Builder-Pattern/Example-2/tests/test_update_room.py
+++ Review-14-Python-Builder-Pattern/Example-2/tests/test_update_room.py
@@ -2,6 +2,7 @@ import pytest
 from hamcrest import *
 
 import update_room
+from house import HouseBuilder
 from room import RoomBuilder
 
 
@@ -37,7 +38,71 @@ def test_build_house():
 
 
 def test_upgrade_flooring():
-    pytest.fail("Test not yet written")
+    house_original = (
+        HouseBuilder()
+        .with_name("House")
+        .with_rooms(
+            [
+                (
+                    RoomBuilder()
+                    .with_name("Laundry Room")
+                    .with_dimensions(8, 4)
+                    .with_flooring("Laminate", 1.95)
+                    .build()
+                ),
+                (
+                    RoomBuilder()
+                    .with_name("Kitchen")
+                    .with_dimensions(20, 12)
+                    .with_flooring("Tile", 3.87)
+                    .build()
+                ),
+                (
+                    RoomBuilder()
+                    .with_name("Storage Room")
+                    .with_dimensions(16, 16)
+                    .with_flooring("Birch Wood", 4.39)
+                    .build()
+                ),
+            ]
+        )
+        .build()
+    )
+
+    house_expected = (
+        HouseBuilder()
+        .with_name("After Stone Bricks")
+        .with_rooms(
+            [
+                (
+                    RoomBuilder()
+                    .with_name("Laundry Room")
+                    .with_dimensions(8, 4)
+                    .with_flooring("Stone Bricks", 12.97)
+                    .build()
+                ),
+                (
+                    RoomBuilder()
+                    .with_name("Kitchen")
+                    .with_dimensions(20, 12)
+                    .with_flooring("Stone Bricks", 12.97)
+                    .build()
+                ),
+                (
+                    RoomBuilder()
+                    .with_name("Storage Room")
+                    .with_dimensions(16, 16)
+                    .with_flooring("Stone Bricks", 12.97)
+                    .build()
+                ),
+            ]
+        )
+        .build()
+    )
+
+    house_upgraded = update_room.upgrade_flooring(house_original)
+
+    assert_that(house_upgraded, is_(equal_to(house_expected)))
 
 
 def test_discount_flooring():
