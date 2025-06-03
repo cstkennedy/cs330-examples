6c3b52b405e7b762916f9fbfd8507ebd0a341998
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2025-06-03 13:30:28 -0400

    Add first round of pyproject.toml/uv configs

--- 06-Python-Shapes/Example-1/tests/__init__.py
+++ 06-Python-Shapes/Example-1/tests/__init__.py
@@ -2,6 +2,5 @@ from shapes.Circle import Circle
 from shapes.EquilateralTriangle import EquilateralTriangle
 from shapes.RightTriangle import RightTriangle
 from shapes.Shape import Shape
-from shapes.ShapeFactory import ShapeFactory
 from shapes.Square import Square
 from shapes.Triangle import Triangle
