a83337b38af06d7d37ab6f5e43c0cd8bd316c3ba
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2026-02-03 12:43:29 -0500

    Add pseudo new type example

---
+++ 04-Java-Unit-Testing/Example-4-NewType/src/main/java/edu/odu/cs/cs350/examples/HtmlColor.java
@@ -0,0 +1,198 @@
+package edu.odu.cs.cs350.examples;
+
+import java.util.Objects;
+
+/**
+ * An HTML Color is represented by a combination of red, green and blue. Each
+ * component (red, green, and blue) must fall in the range [0, 255].
+ *
+ * The methods in this class clamp all values to the range [0, 255].
+ */
+public class HtmlColor implements Cloneable
+{
+    /**
+     * Represents a color component (red, green, or blue).
+     */
+    public static class Component
+    {
+        /**
+         * Clamp is an implementation of the mathematical operation that takes a
+         * value and ensures is falls within a specified range.
+         *
+         *   - Any value that is less than 0 will be replaced with zero.
+         *   - Any value that is greater than 255 will be replaced with 255.
+         *
+         * @param val raw value
+         *
+         * @return clamped value
+         */
+        public static int clamp(int val)
+        {
+            return Math.min(Math.max(0, val), 255);
+        }
+
+        // Not implemented
+        public static Component from(int raw_val)
+        {
+            return null;
+        }
+
+        public static Component from_clamped(int raw_val)
+        {
+            final int clamped_val = Component.clamp(raw_val);
+
+            return new Component(clamped_val);
+        }
+
+        private int val;
+
+        private Component()
+        {
+            this.val = 0;
+        }
+
+        private Component(int sanitized_val)
+        {
+            this.val = sanitized_val;
+        }
+
+        public int value()
+        {
+            return this.val;
+        }
+    }
+
+    private Component red;   ///< red color component [0,255]
+    private Component green; ///< green color component [0,255]
+    private Component blue;  ///< blue color component [0,255]
+
+    /**
+     * Construct an HTML Color with all
+     * attributes set to 0 (i.e., black, #000000)
+     */
+    public HtmlColor()
+    {
+        this.red = Component.from_clamped(0);
+        this.green = Component.from_clamped(0);
+        this.blue = Component.from_clamped(0);
+    }
+
+    /**
+     * Construct an HTML Color
+     *
+     * @param rVal red value
+     * @param gVal green value
+     * @param bVal blue value
+     *
+     * All values are clamped to the range [0, 255]
+     */
+    public HtmlColor(Component rVal, Component gVal, Component bVal)
+    {
+        this.red = rVal;
+        this.green = gVal;
+        this.blue = bVal;
+    }
+
+    /**
+     * Retrieve the red component
+     */
+    public Component getRed()
+    {
+        return this.red;
+    }
+
+    /**
+     * Set the red component
+     *
+     * @param val new value
+     */
+    public void setRed(Component val)
+    {
+        this.red = val;
+    }
+
+    /**
+     * Retrieve the green component
+     */
+    public Component getGreen()
+    {
+        return this.green;
+    }
+
+    /**
+     * Set the green component
+     *
+     * @param val new value
+     */
+    public void setGreen(Component val)
+    {
+        this.green = val;
+    }
+
+    /**
+     * Retrieve the blue component
+     */
+    public Component getBlue()
+    {
+        return this.blue;
+    }
+
+    /**
+     * Set the blue component
+     *
+     * @param v new value
+     */
+    public void setBlue(Component val)
+    {
+        this.blue = val;
+    }
+
+    /**
+     * Two colors are considered equal if their red, green, and blue
+     * components are equivalent.
+     */
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof HtmlColor)) {
+            return false;
+        }
+
+        HtmlColor rhs = (HtmlColor) obj;
+
+        return this.red.value() == rhs.red.value()
+            && this.green.value() == rhs.green.value()
+            && this.blue.value() == rhs.blue.value();
+    }
+
+    /**
+     * Hash codes are computed using red, green, and blue
+     */
+    public int hashCode()
+    {
+        return Objects.hash(this.red.value(), this.green.value(), this.blue.value());
+    }
+
+    /**
+     * Return a (deep) copy of this object.
+     */
+    @Override
+    public Object clone()
+    {
+        return new HtmlColor(this.red, this.green, this.blue);
+    }
+
+    /**
+     * Colors are represented in the form #RRGGBB where RR, GG, and BB are the
+     * red, green and blue components, respectively, in base 16 (hexadecimal).
+     */
+    public String toString()
+    {
+        return String.format(
+            "#%02X%02X%02X",
+            red.value(),
+            green.value(),
+            blue.value()
+        ).toUpperCase();
+    }
+
+}
---
+++ 04-Java-Unit-Testing/Example-4-NewType/src/test/java/edu/odu/cs/cs350/examples/TestHtmlColor.java
@@ -0,0 +1,198 @@
+package edu.odu.cs.cs350.examples;
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import org.hamcrest.core.IsNull;
+
+/**
+ * 1 - Does this piece of code perform the operations
+ *     it was designed to perform?
+ *
+ * 2 - Does this piece of code do something it was not
+ *     designed to perform?
+ *
+ * 1 Test per mutator
+ */
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class TestHtmlColor {
+    private HtmlColor black;
+    private HtmlColor white;
+    private HtmlColor red;
+    private HtmlColor green;
+    private HtmlColor blue;
+    private HtmlColor rColor; // "random" color
+
+    @BeforeEach
+    public void setUp()
+    {
+        black = new HtmlColor();
+        white = new HtmlColor(
+            HtmlColor.Component.from_clamped(255),
+            HtmlColor.Component.from_clamped(255),
+            HtmlColor.Component.from_clamped(255)
+        );
+        red = new HtmlColor(
+            HtmlColor.Component.from_clamped(255),
+            HtmlColor.Component.from_clamped(0),
+            HtmlColor.Component.from_clamped(0)
+        );
+        green = new HtmlColor(
+            HtmlColor.Component.from_clamped(0),
+            HtmlColor.Component.from_clamped(255),
+            HtmlColor.Component.from_clamped(0)
+        );
+        blue = new HtmlColor(
+            HtmlColor.Component.from_clamped(0),
+            HtmlColor.Component.from_clamped(0),
+            HtmlColor.Component.from_clamped(255)
+        );
+        rColor = new HtmlColor(
+            HtmlColor.Component.from_clamped(7),
+            HtmlColor.Component.from_clamped(62),
+            HtmlColor.Component.from_clamped(55)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-100, -5, -1, 0})
+    public void testClampBelowRange(int value)
+    {
+        assertThat(HtmlColor.Component.clamp(value), is(0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10, 100, 200, 250, 254, 255})
+    public void testClampInRange(int value)
+    {
+        assertThat(HtmlColor.Component.clamp(value), is(value));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {256, 300, 700, 1000})
+    public void testClampAboveRange(int value)
+    {
+        assertThat(HtmlColor.Component.clamp(value), is(255));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-100, -5, -1, 0})
+    public void testComponentFromClampedBelowRange(int value)
+    {
+        assertThat(HtmlColor.Component.from_clamped(value).value(), is(0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10, 100, 200, 250, 254, 255})
+    public void testComponentFromClampedInRange(int value)
+    {
+        assertThat(HtmlColor.Component.from_clamped(value).value(), is(value));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {256, 300, 700, 1000})
+    public void testCompoentFromClampedAboveRange(int value)
+    {
+        assertThat(HtmlColor.Component.from_clamped(value).value(), is(255));
+    }
+
+    @Test
+    public void testDefaultConstructor()
+    {
+        HtmlColor color = new HtmlColor();
+
+        assertThat(color.getRed().value(), is(0));
+        assertThat(color.getBlue().value(), is(0));
+        assertThat(color.getGreen().value(), is(0));
+
+        assertThat(color.hashCode(), is(black.hashCode()));
+        assertThat(color.toString(), equalTo("#000000"));
+    }
+
+    @Test
+    public void testNonDefaultConstructor()
+    {
+        HtmlColor color = new HtmlColor(
+            HtmlColor.Component.from_clamped(7),
+            HtmlColor.Component.from_clamped(62),
+            HtmlColor.Component.from_clamped(55)
+        );
+
+        assertThat(color.getRed().value(), is(7));
+        assertThat(color.getGreen().value(), is(62));
+        assertThat(color.getBlue().value(), is(55));
+
+        assertThat(rColor.hashCode(), is(color.hashCode()));
+
+        String base16String = String.format(
+            "#%02X%02X%02X",
+            color.getRed().value(),
+            color.getGreen().value(),
+            color.getBlue().value()).toUpperCase();
+
+        assertThat(color.toString(), equalTo(base16String));
+    }
+
+    @Test
+    public void testSetRed()
+    {
+        HtmlColor color = (HtmlColor) black.clone();
+        int oldHashCode = color.hashCode();
+
+        color.setRed(HtmlColor.Component.from_clamped(100));
+
+        assertThat(color.getRed().value(), is(100));
+        assertThat(color.getBlue().value(), is(0));
+        assertThat(color.getGreen().value(), is(0));
+
+        assertThat(color, is(equalTo(color)));
+        assertThat(color, is(not(equalTo(black))));
+        assertThat(color.hashCode(), not(oldHashCode));
+    }
+
+    @Test
+    public void testSetGreen()
+    {
+        HtmlColor color = (HtmlColor) blue.clone();
+        int oldHashCode = color.hashCode();
+
+        color.setGreen(HtmlColor.Component.from_clamped(100));
+
+        assertThat(color.getRed().value(), is(0));
+        assertThat(color.getBlue().value(), is(255));
+        assertThat(color.getGreen().value(), is(100));
+
+        assertThat(color, is(not(equalTo(blue))));
+        assertThat(color.hashCode(), not(oldHashCode));
+    }
+
+    @Test
+    public void testSetBlue()
+    {
+        HtmlColor color = (HtmlColor) white.clone();
+        int oldHashCode = color.hashCode();
+
+        color.setBlue(HtmlColor.Component.from_clamped(100));
+
+        assertThat(color.getRed().value(), is(255));
+        assertThat(color.getBlue().value(), is(100));
+        assertThat(color.getGreen().value(), is(255));
+
+        assertThat(color, is(not(equalTo(white))));
+        assertThat(color.hashCode(), not(oldHashCode));
+        assertThat(color.toString(), equalTo("#FFFF64"));
+
+        //assertTrue(color.toString().contains("64"));
+        assertThat(color.toString(), containsString("64"));
+        assertThat(color.toString(), containsString("FFFF"));
+    }
+}
